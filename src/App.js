9d6b1e5e1ece078b7936d1aad6f2f0c152231fb3
Emith Randiv <randivemith.2003@gmail.com>
2025-03-17 00:24:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Tabs, Tab, Box, Container, Grid } from "@mui/material";
 import logo from './logo.svg';
-import './App.css';
+import { Home, About, Services, Pickups, Contact } from './PublicPage';
+import theme from "./theme";
+import { Link as ScrollLink } from "react-scroll";
+import ScrollToTop from "./ScrollToTop";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Navigation() {
+    const [value, setValue] = React.useState(0);
+
+    return (
+        <AppBar position="static" color="none" elevation={0}>
+            <Toolbar>
+                <Grid container alignItems="center" justifyContent="space-between" sx={{ marginRight: 3, marginLeft: 3, marginTop: 2, }}>
+                <Grid item>
+                    <img src={logo} alt="My Logo" style={{ height: '100px' }} />
+                </Grid>
+                <Grid item sx={{ textAlign: "center" }}>
+                    <Typography variant="h4" sx={{ fontWeight: 'bold', color: theme.palette.darkgreen.main }}>
+                        Green Guard Solutions
+                    </Typography>
+                    <Typography variant="h6" sx={{ color: theme.palette.darkgreen.main, }}>
+                        The Sustainable E-Waste Management System
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant="body1" sx={{ fontWeight: 'bold', }}>Contact: +94 456 7890</Typography>
+                </Grid>
+                </Grid>
+            </Toolbar>
+
+            <Toolbar sx={{ marginRight: 3, marginLeft: 3, marginBottom: 2, }}>
+                <Tabs 
+                    value={value} 
+                    onChange={(e, newValue) => setValue(newValue)} 
+                    centered
+                    textColor="secondary"
+                    indicatorColor="secondary"
+                >
+                <Tab label="Home" component={Link} to="/" />
+                <Tab label="Our Services" component={ScrollLink} to="services" smooth={true} duration={500} />
+                <Tab label="About Us" component={ScrollLink} to="about" smooth={true} duration={500} />
+                <Tab label="Schedule Pickups" component={ScrollLink} to="pickups" smooth={true} duration={500} />
+                <Tab label="Contact us" component={ScrollLink} to="contact" smooth={true} duration={500} />
+                </Tabs>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
-export default App;
+export default function App() {
+    return (
+        <Router>
+            <Navigation />
+            <Container>
+                <Box my={4}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/pickups" element={<Pickups />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+                </Box>
+            </Container>
+            <ScrollToTop />
+        </Router>
+    );
+}
---
+++ src/publicSchedulePickupForm.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import theme from './theme';
+import { Container, Grid, TextField, Typography } from '@mui/material';
+
+export function PickupForm() {
+    return (
+        <Container sx={{ bgcolor: theme.palette.darkgreen.main, pb: 4, }}>
+            <Typography 
+                variant='h4' 
+                align='center' 
+                fontWeight={'bold'}
+                sx={{ 
+                    mt: 3, 
+                    mb: 2, 
+                    pt: 4,
+                    color: theme.palette.orange.main, }}
+            >
+                Schedule Your Pickup
+            </Typography>
+
+            <Grid container spacing={2} sx={{ width: "80%", margin: "auto", pb: 4, }}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="name"
+                        label="Your Name Mr/Mrs"
+                        fullWidth
+                        InputProps={{
+                            sx: { backgroundColor: theme.palette.white.main, borderRadius: 1 },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="phone"
+                        label="Phone Number"
+                        fullWidth
+                        InputProps={{
+                            sx: { backgroundColor: theme.palette.white.main, borderRadius: 1 },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="address"
+                        label="Pickup Address"
+                        fullWidth
+                        InputProps={{
+                            sx: { backgroundColor: theme.palette.white.main, borderRadius: 1 },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        id="message"
+                        label="Message"
+                        fullWidth
+                        InputProps={{
+                            sx: { backgroundColor: theme.palette.white.main, borderRadius: 1 },
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="date"
+                        label="Pickup Date"
+                        fullWidth
+                        InputProps={{
+                            sx: { backgroundColor: theme.palette.white.main, borderRadius: 1 },
+                        }}
+                    />
+                </Grid>
+
+            </Grid>
+        </Container>
+    );
+}
